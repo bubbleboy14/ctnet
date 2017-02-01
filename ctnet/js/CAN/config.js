b25fe664162665b0012256339a02d10e8d830547
bubbleboy14 <mario.balibrera@gmail.com>
2017-02-01 01:59:43 -0800

    another comment regarding old scrambler stuff that should be cleared out

--- ctnet/js/CAN/config.js
+++ ctnet/js/CAN/config.js
@@ -1,5 +1,6 @@
 CT.require("core");
 var c = CAN.config = core.config.ctnet;
 c.mobile.page = c.mobile.menus[CT.info.page];
+// get rid of scrambler bs below -- now built into ct (properly)
 c.scramlen = c.scrambler.length;
 c.scramlenh = c.scramlen / 2;
