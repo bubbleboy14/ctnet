ade133503e94f5851104671e4dcd4b3c41a494fb
bubbleboy14 <mario.balibrera@gmail.com>
2017-02-01 20:15:10 -0800

    changed scrambler reference -- will ultimately remove all scrambling/encoding -related code at this level (this plugin)

--- ctnet/js/CAN/config.js
+++ ctnet/js/CAN/config.js
@@ -2,5 +2,5 @@ CT.require("core");
 var c = CAN.config = core.config.ctnet;
 c.mobile.page = c.mobile.menus[CT.info.page];
 // get rid of scrambler bs below -- now built into ct (properly)
-c.scramlen = c.scrambler.length;
+c.scramlen = CT.net._scrambler.length;
 c.scramlenh = c.scramlen / 2;
