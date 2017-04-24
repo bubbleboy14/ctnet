9255b288c046359a0ca917f5de457b5c73c49469
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-24 14:33:00 -0700

    newaccount -- moved imports to loader

---
+++ ctnet/js/pages/newaccount.js
@@ -0,0 +1,8 @@
+CT.require("CT.align");
+CT.require("CT.dom");
+CT.require("CAN.config");
+CT.require("CAN.cookie");
+CT.require("CAN.frame");
+CT.require("CAN.search");
+
+onload = CAN.frame.basic;
