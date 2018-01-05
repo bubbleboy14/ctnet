2bbd19210c2f88acd49297b1658681031f23d560
bubbleboy14 <mario.balibrera@gmail.com>
2018-01-05 07:39:10 -0800

    clb tweakz

--- ctnet/js/CAN/widget/chatterlightbox.js
+++ ctnet/js/CAN/widget/chatterlightbox.js
@@ -8,7 +8,7 @@ CAN.widget.chatterlightbox = {
 	        CAN.widget.stream.comment(cbox, null, items.reverse(), false, true, "full");
 	        CT.dom.inputEnterCallback(CT.dom.richInput(talkBack, null, null, null, null,
 	        	["what do you think we should discuss?", "breaking news?", "what's the latest?",
-	        	"your turn! what's next on the global thought stream?"], null, "w1 h100p"), function(val) {
+	        	"your turn! what's next on the global thought stream?"], null, "w1 h50p"), function(val) {
 		        	CT.storage.set("gts", val.trim());
 		        	location = "/community.html#!Stream";
 		        });
@@ -21,11 +21,16 @@ CAN.widget.chatterlightbox = {
 						CT.dom.div("Civil Action Network is your source for peaceful activism and the democratic exchange of ideas. Every perspective counts - make yourself heard!", "padded"),
 						CT.dom.div("true democracy", "biggerer bold right bordertop"),
 						CT.dom.div([
-							CT.dom.span("Our goal is to increase the degree to which all of us can have a positive, creative impact on ourselves, each other, our neighborhoods, and our world. This means having a"),
+							CT.dom.span("Our goal is to increase the degree to which all of us can have a positive, creative impact on ourselves, each other,"),
 							CT.dom.pad(),
-							CT.dom.link("direct say", null, "/referenda.html"),
+							CT.dom.link("our neighborhoods", null, "/community.html#!Map"),
+							CT.dom.span(", and our world. This means having a direct say in projects and policies that affect us (as any genuine human being would expect).")
+						], "padded"),
+						CT.dom.div([
+							CT.dom.span("With"),
 							CT.dom.pad(),
-							CT.dom.span("in projects and policies that affect us (as any genuine human being would expect). With CAN Referenda, we all riff on each other's ideas, branching proposals and even voting on branches individually before settling upon a final version (for further voting :).")
+							CT.dom.link("CAN Referenda", null, "/referenda.html"),
+							CT.dom.span(", we all riff on each other's ideas, branching proposals and even voting on branches individually before settling upon a final version (for further voting :).")
 						], "padded"),
 						CT.dom.div("real media", "biggerer bold right bordertop"),
 						CT.dom.div([
@@ -42,7 +47,10 @@ CAN.widget.chatterlightbox = {
 							CT.dom.span(", you're helping us all question mainstream narratives.")
 						], "padded"),
 						CT.dom.div([
-							CT.dom.span("Meanwhile, our system catalogs your findings for"),
+							CT.dom.span("Meanwhile, our system catalogs your contributions (including"),
+							CT.dom.pad(),
+							CT.dom.link("book recommendations, cool pictures, inspiring quotes, position papers, more free-form essays, and more", null, "/recommendations.html"),
+							CT.dom.span(") for"),
 							CT.dom.pad(),
 							CT.dom.link("fellow researchers", null, "/cases.html"),
 							CT.dom.span(". The best part is, member-initiated investigations are crowd-sourced, with other members submitting evidence as they encounter it.")
@@ -55,13 +63,13 @@ CAN.widget.chatterlightbox = {
 						], "padded"),
 						CT.dom.div("grassroots cooperation", "biggerer bold right bordertop"),
 						CT.dom.div([
-							CT.dom.span("This means communication. Want to talk to anyone else on the site? Click 'expand' in the bottom-right corner of the page to see who's logged in. Prefer video chat? Just click 'Give It A Whirl!' on the"),
+							CT.dom.span("Cooperation relies upon communication. Want to talk to anyone else on the site? Click 'expand' in the bottom-right corner of the page to see who's logged in. Prefer video chat? Just click 'Give It A Whirl!' on the"),
 							CT.dom.pad(),
 							CT.dom.link("community page", null, "/community.html"),
 							CT.dom.span(", which by the way is also where members post and peruse upcoming events.")
 						], "padded"),
 						CT.dom.div([
-							CT.dom.span("This also means"),
+							CT.dom.span("High-level coordination within and among action groups is enhanced by"),
 							CT.dom.pad(),
 							CT.dom.link("secure organizational tools", null, "/participate.html#ActionGroups"),
 							CT.dom.pad(),
