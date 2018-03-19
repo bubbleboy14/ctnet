bc8905cbc77864f8c3f56f9dd7060d64800639a8
bubbleboy14 <mario.balibrera@gmail.com>
2018-03-18 21:40:20 -0700

    added trending and forum to home page mobile menu

--- ctnet/js/config.js
+++ ctnet/js/config.js
@@ -15,160 +15,170 @@
 	},
 	"mobile": {
 		"resize": {
-		    "stretched": {
-		        "alt": "Stretch page to fill your screen",
-		        "img": "/img/screen-stretched.png"
-		    },
-		    "mobile": {
-		        "alt": "Zoom into content",
-		        "img": "/img/screen-mobile.png"
-		    },
-		    "normal": {
-		        "alt": "Unzoom page",
-		        "img": "/img/screen-normal.png"
-		    }
+			"stretched": {
+				"alt": "Stretch page to fill your screen",
+				"img": "/img/screen-stretched.png"
+			},
+			"mobile": {
+				"alt": "Zoom into content",
+				"img": "/img/screen-mobile.png"
+			},
+			"normal": {
+				"alt": "Unzoom page",
+				"img": "/img/screen-normal.png"
+			}
 		},
 		"menus": {
-		    "alternatives": {
-		        "participate": {
-		            "page": "participate",
-		            "icon": "opinions_and_ideas"
-		        },
-		        "login": {
-		            "page": "login",
-		            "icon": "inout"
-		        }
-		    },
-		    "bottom": [
-		        {
-		            "page": "home",
-		            "icon": "world"
-		        },
-		        {
-		            "page": "news",
-		            "icon": "articles"
-		        },
-		        {
-		            "page": "video",
-		            "icon": "videos"
-		        },
-		        {
-		            "name": "laws",
-		            "page": "referenda",
-		            "icon": "referenda"
-		        },
-		        {
-		            "name": "people",
-		            "page": "community",
-		            "icon": "action_groups"
-		        },
-		        {
-		            "name": "hot stuff",
-		            "page": "recommendations",
-		            "icon": "approver"
-		        },
-		        {
-		            "page": "cases",
-		            "icon": "cases"
-		        },
-		        {
-		            "page": "about",
-		            "icon": "moderator"
-		        }
-		    ],
-		    "top": [
-		        {
-		            "name": "menu",
-		            "firstClass": "bigpadded",
-		            "icon": "authenticator"
-		        },
-		        {
-		            "name": "content",
-		            "firstClass": "maxwidthoverride",
-		            "icon": "position_papers"
-		        },
-		        {
-		            "name": "side bar",
-		            "firstClass": "rightcolumn",
-		            "icon": "Introduction"
-		        }
-		    ],
-		    "home": {
-		        "top": [
-		            {
-		                "name": "challenge",
-		                "id": "tripleChallenge",
-		                "icon": "founder"
-		            },
-		            {
-		                "name": "change",
-		                "id": "howChange",
-		                "icon": "world"
-		            },
-		            {
-		                "name": "news",
-		                "id": "latestNews",
-		                "icon": "articles"
-		            },
-		            {
-		                "name": "video",
-		                "id": "latestVideos",
-		                "icon": "videos"
-		            },
-		            {
-		                "name": "referenda",
-		                "id": "latestRefs",
-		                "icon": "referenda"
-		            },
-		            {
-		                "name": "books",
-		                "id": "latestBooks",
-		                "icon": "books"
-		            },
-		            {
-		                "name": "thoughts",
-		                "id": "latestThoughts",
-		                "icon": "thought_stream"
-		            }
-		        ]
-		    },
-		    "search": {
-		        "top": [
-		            {
-		                "name": "side bar",
-		                "firstClass": "rightcolumn",
-		                "icon": "Introduction"
-		            },
-		            {
-		                "name": "search results",
-		                "id": "searchcontent",
-		                "icon": "position_papers"
-		            }
-		        ]
-		    },
-		    "about": {
-		        "menu": "bigtabbed"
-		    },
-		    "cases": {
-		        "menu": "bigtabbed"
-		    },
-		    "login": {
-		        "menu": "bigtabbed"
-		    },
-		    "participate": {
-		        "menu": "bigtabbed"
-		    },
-		    "profile": {
-		        "menu": "leftcolumn"
-		    },
-		    "community": {
-		        "menu": "bigtabbed",
-		        "content": "maxwidthoverridestream"
-		    },
-		    "recommendations": {
-		        "menu": "bigtabbed",
-		        "content": "maxwidthoverridestream"
-		    }
+			"alternatives": {
+				"participate": {
+					"page": "participate",
+					"icon": "opinions_and_ideas"
+				},
+				"login": {
+					"page": "login",
+					"icon": "inout"
+				}
+			},
+			"bottom": [
+				{
+					"page": "home",
+					"icon": "world"
+				},
+				{
+					"page": "news",
+					"icon": "articles"
+				},
+				{
+					"page": "video",
+					"icon": "videos"
+				},
+				{
+					"name": "laws",
+					"page": "referenda",
+					"icon": "referenda"
+				},
+				{
+					"name": "people",
+					"page": "community",
+					"icon": "action_groups"
+				},
+				{
+					"name": "hot stuff",
+					"page": "recommendations",
+					"icon": "approver"
+				},
+				{
+					"page": "cases",
+					"icon": "cases"
+				},
+				{
+					"page": "about",
+					"icon": "moderator"
+				}
+			],
+			"top": [
+				{
+					"name": "menu",
+					"firstClass": "bigpadded",
+					"icon": "authenticator"
+				},
+				{
+					"name": "content",
+					"firstClass": "maxwidthoverride",
+					"icon": "position_papers"
+				},
+				{
+					"name": "side bar",
+					"firstClass": "rightcolumn",
+					"icon": "Introduction"
+				}
+			],
+			"home": {
+				"top": [
+					{
+						"name": "challenge",
+						"id": "tripleChallenge",
+						"icon": "founder"
+					},
+					{
+						"name": "change",
+						"id": "howChange",
+						"icon": "world"
+					},
+					{
+						"name": "news",
+						"id": "latestNews",
+						"icon": "articles"
+					},
+					{
+						"name": "video",
+						"id": "latestVideos",
+						"icon": "videos"
+					},
+					{
+						"name": "referenda",
+						"id": "latestRefs",
+						"icon": "referenda"
+					},
+					{
+						"name": "books",
+						"id": "latestBooks",
+						"icon": "books"
+					},
+					{
+						"name": "thoughts",
+						"id": "latestThoughts",
+						"icon": "thought_stream"
+					},
+					{
+						"icon": "approver",
+						"name": "trending",
+						"id": "forum"
+					},
+					{
+						"icon": "conversations",
+						"name": "forum",
+						"id": "topz"
+					}
+				]
+			},
+			"search": {
+				"top": [
+					{
+						"name": "side bar",
+						"firstClass": "rightcolumn",
+						"icon": "Introduction"
+					},
+					{
+						"name": "search results",
+						"id": "searchcontent",
+						"icon": "position_papers"
+					}
+				]
+			},
+			"about": {
+				"menu": "bigtabbed"
+			},
+			"cases": {
+				"menu": "bigtabbed"
+			},
+			"login": {
+				"menu": "bigtabbed"
+			},
+			"participate": {
+				"menu": "bigtabbed"
+			},
+			"profile": {
+				"menu": "leftcolumn"
+			},
+			"community": {
+				"menu": "bigtabbed",
+				"content": "maxwidthoverridestream"
+			},
+			"recommendations": {
+				"menu": "bigtabbed",
+				"content": "maxwidthoverridestream"
+			}
 		}
 	}
 }
