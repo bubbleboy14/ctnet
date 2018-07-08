631250ac50fc4ce052f9a288c451dd80d503101f
bubbleboy14 <mario.balibrera@gmail.com>
2018-07-08 07:42:36 -0700

    CAN.media.news: updated various CT.dom.node() references -> div() or span()

--- ctnet/js/CAN/media/news.js
+++ ctnet/js/CAN/media/news.js
@@ -3,16 +3,16 @@ CAN.media.news = {
 	"current": null,
 	"build": function(d, vindex, howmuch, nocat, v) {
 	    v = v || CAN.media.loader.args.news;
-	    var n = howmuch == "slider" && CT.dom.node() || CT.dom.node("", "div",
+	    var n = howmuch == "slider" && CT.dom.node() || CT.dom.div("",
 	        howmuch == "teaser" && "smallbottompadded" || "bottompadded");
 	    howmuch = howmuch || "full";
 
 	    if (howmuch == "full" || howmuch == "intro") {
 	        if (howmuch == "intro")
-	            n.appendChild(CT.dom.node(CT.dom.link(d.title, function() {
-	                v.newMediaViewMoreCb(d); }), "div", "newstitletext"));
+	            n.appendChild(CT.dom.div(CT.dom.link(d.title, function() {
+	                v.newMediaViewMoreCb(d); }), "newstitletext"));
 	        else
-	            n.appendChild(CT.dom.node(d.title, "div", "big bold red"));
+	            n.appendChild(CT.dom.div(d.title, "big bold red"));
 	        var cclass = " categoriedbox";
 	        if (!nocat) {
 	            for (var i = 0; i < d.category.length; i++)
@@ -26,9 +26,9 @@ CAN.media.news = {
 	            cclass += " mediumgraphic lmargimg";
 	        n.className += cclass;
 	        var a = CT.data.get(d.user);
-	        var byline = CT.dom.node("", "div", "newstitle small italic");
-	        byline.appendChild(CT.dom.node("posted " + (d.date
-	        	|| "(now)") + ", by ", "span", "gray"));
+	        var byline = CT.dom.div("", "newstitle small italic");
+	        byline.appendChild(CT.dom.span("posted " + (d.date
+	        	|| "(now)") + ", by ", "gray"));
 	        byline.appendChild(CT.dom.link(a.firstName + " " + a.lastName,
 	            null, "/profile.html?u=" + CAN.cookie.flipReverse(a.key), "gray"));
 	        n.appendChild(byline);
@@ -50,7 +50,7 @@ CAN.media.news = {
 	    }
 
 	    if (howmuch == "teaser" || howmuch == "result" || howmuch == "slider") {
-	        var tnode = CT.dom.node(d.title, "div",
+	        var tnode = CT.dom.div(d.title,
 	            (howmuch=="teaser")&&"cutoff64"||"big bold bottompadded");
 	        n.appendChild(tnode);
 	        if (howmuch == "slider") {
@@ -62,7 +62,7 @@ CAN.media.news = {
 	            n.appendChild(CT.dom.node(CT.parse.shortened(d.body)));
 	        n.appendChild(CT.dom.node(CT.dom.link("FULL STORY",
 	            function() { v.newMediaViewMoreCb(d); })));
-	        n.appendChild(CT.dom.node("", "div", "clearnode"));
+	        n.appendChild(CT.dom.div("", "clearnode"));
 	        return n;
 	    }
 
@@ -73,7 +73,7 @@ CAN.media.news = {
 	        CAN.media.news.current = d;
 	        !nocat && CT.dom.showHide(CAN.media.news.inviteButton.parentNode, true);
 	        // body
-	        n.appendChild(CT.dom.node(b, "div", "newsbodytext"));
+	        n.appendChild(CT.dom.div(b, "newsbodytext"));
 	        //categories
 	        if (!nocat)
 	            n.appendChild(CAN.categories.listing(d.category));
@@ -85,7 +85,7 @@ CAN.media.news = {
 	            n.appendChild(CAN.media.video.build(CT.data.get(d.video[i]),
 	                null, true));
 	        if (!nocat) {
-	            var convonode = CT.dom.node("loading conversation...", "div", "red bordertop");
+	            var convonode = CT.dom.div("loading conversation...", "red bordertop");
 	            n.appendChild(convonode);
 	            CAN.widget.conversation.load(v.uid, d.conversation, convonode, d.key);
 	        }
@@ -94,7 +94,7 @@ CAN.media.news = {
 	        var itxt = b.slice(0, b.indexOf("<br>", 100));
 	        if (itxt.length > 800)
 	            itxt = itxt.slice(0, 800) + "...";
-	        var nbt = CT.dom.node(itxt, "div", "newsbodytext");
+	        var nbt = CT.dom.div(itxt, "newsbodytext");
 	        if (nbt.firstChild.nodeName == "TABLE")
 	            nbt.firstChild.style.maxWidth = "200px";
 	        n.appendChild(nbt);
@@ -119,9 +119,9 @@ CAN.media.news = {
 	},
 	"evidence": function(d) {
 	    var n = CT.dom.node();
-	    n.appendChild(CT.dom.node(CT.dom.link(d.title, function() {
+	    n.appendChild(CT.dom.div(CT.dom.link(d.title, function() {
 	        document.location = "/news.html#!" + CAN.cookie.flipReverse(d.key); }),
-	        "div", "newstitletext"));
+	        "newstitletext"));
 	    n.appendChild(CT.dom.node(CT.parse.shortened(d.body, 900)));
 	    return n;
 	},
