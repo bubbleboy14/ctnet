4811af66a067cc5d3899891d07c3cfd313b041e2
bubbleboy14 <mario.balibrera@gmail.com>
2018-08-16 20:27:38 -0700

    feed loader -- 1st cut

---
+++ ctnet/js/pages/feed.js
@@ -0,0 +1,28 @@
+CT.require("CT.dom");
+
+var empty, chunk = 15, offset = 0,
+	uid = location.hash.slice(2);
+refill = function() {
+	!empty && CT.net.post({
+		path: "/get",
+		spinner: true,
+		params: {
+			uid: uid,
+			chunk: chunk,
+			offset: offset,
+			gtype: "skinfo"
+		},
+		cb: function(fulld) {
+			var skin = fulld.skin,
+				data = fulld.data;
+			if (skin && offset == chunk) { // first request
+				CT.dom.addStyle(skin.css);
+				CT.dom.setContent("feed_title", skin.title);
+			}
+			CT.dom.addContent("feed", JSON.stringify(data));
+			offset += chunk;
+			empty = data.length != chunk;
+		}
+	});
+};
+refill();
