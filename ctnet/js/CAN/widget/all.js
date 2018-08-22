96610d0510f0fe8faa024703a7426e37df0744cb
bubbleboy14 <mario.balibrera@gmail.com>
2018-08-21 21:38:31 -0700

    added skinner to widget loader

--- ctnet/js/CAN/widget/all.js
+++ ctnet/js/CAN/widget/all.js
@@ -5,5 +5,6 @@ CT.require("CAN.widget.conversation");
 CT.require("CAN.widget.invite");
 CT.require("CAN.widget.map");
 CT.require("CAN.widget.share");
+CT.require("CAN.widget.skinner");
 CT.require("CAN.widget.slider");
 CT.require("CAN.widget.stream");
