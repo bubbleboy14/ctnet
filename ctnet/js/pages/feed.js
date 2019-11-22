ac5e602c1b31858176b5faa46d205b58960fe52f
bubbleboy14 <mario.balibrera@gmail.com>
2019-11-22 07:52:07 -0800

    feed page loader: support global feed

--- ctnet/js/pages/feed.js
+++ ctnet/js/pages/feed.js
@@ -17,6 +17,100 @@ var empty, chunk = 15, offset = 0,
 	hash = location.hash.slice(2),
 	plink = "/profile.html?u=" + hash,
 	uid = CAN.cookie.flipReverse(hash);
+
+var process = function(fulld) {
+	var skin = fulld.skin,
+		data = fulld.data;
+	if (!(offset || data.length))
+		window.location = plink;
+	CT.data.addSet(data);
+	if (!offset) { // first request
+		if (uid)
+			CT.dom.addContent(document.body, CT.dom.link("profile", null, plink,
+				"fixed cbl mosthigh biggerest whitelink nodecoration hoverglow"));
+		else if (!skin) {
+			skin = {
+				chat: true,
+				chatter: true,
+				color: "#010068",
+				title: "Global Feed"
+			};
+		}
+		if (skin) {
+			skin.font && CT.dom.addStyle(null, null, {
+				body: {
+					"font-family": skin.font
+				}
+			});
+			skin.color && CT.dom.addStyle(null, null, {
+				body: {
+					color: skin.color
+				}
+			});
+			skin.background && CT.dom.addStyle(null, null, {
+				body: {
+					background: skin.background
+				}
+			});
+			skin.img && CT.dom.addStyle(null, null, {
+				body: {
+					background: "url(" + skin.img + ")",
+					"background-size": "cover"
+				}
+			});
+			skin.css && CT.dom.addStyle(skin.css);
+			if (skin.title) {
+				var tnode = CT.dom.div(skin.title, "pointer");
+				tnode.onclick = function() { location = "/home.html"; };
+				CT.dom.setContent("feed_title", tnode);
+				CT.dom.setContent(CT.dom.tag("title")[0], skin.title);
+			}
+			if (skin.chatter) {
+				var chchunk = 6, chparams = {
+					uid: uid,
+					offset: 0,
+					number: chchunk,
+					gtype: "media",
+					mtype: "comment"
+				};
+				CT.dom.id("feed").classList.add("w4-5");
+				CT.dom.show("feed_chatter");
+				CT.net.post("/get", chparams, null, function(items) {
+					var fcnode = CT.dom.id("feed_chatter"),
+						fcrefiller = CT.dom.div();
+					fcrefiller.on("visible", function() {
+						chparams.offset += chchunk;
+						CT.dom.remove(fcrefiller);
+						CT.net.post("/get", chparams, null, function(cz) {
+							for (var i = 0; i < cz.length; i++)
+								fcnode.addNode(cz[i], true);
+							if (cz.length == chchunk)
+								CT.dom.addContent(fcnode, fcrefiller);
+						});
+					});
+					CAN.widget.stream.comment(fcnode, null,
+						items.reverse(), false, true, "full");
+					CT.dom.addContent(fcnode, fcrefiller);
+				});
+			}
+			if (skin.chat) {
+				CT.dom.loadAllNode(true);
+				CAN.frame.loadSiteWideChat();
+			}
+		}
+	}
+	CT.dom.addContent("feed", data.map(CAN.media.loader.contentNode));
+	empty = data.length != chunk;
+	if (empty)
+		CT.dom.remove("refiller");
+	else if (!offset) { // first request
+		CT.dom.addContent(document.body, CT.dom.div(null, null, "refiller", {
+			onvisible: refill
+		}));
+	}
+	offset += chunk;
+};
+
 var refill = function() {
 	!empty && CT.net.post({
 		path: "/get",
@@ -27,87 +121,7 @@ var refill = function() {
 			offset: offset,
 			gtype: "skinfo"
 		},
-		cb: function(fulld) {
-			var skin = fulld.skin,
-				data = fulld.data;
-			if (!(offset || data.length))
-				window.location = plink;
-			CT.data.addSet(data);
-			if (!offset) { // first request
-				CT.dom.addContent(document.body, CT.dom.link("profile", null, plink,
-					"fixed cbl mosthigh biggerest whitelink nodecoration hoverglow"));
-				if (skin) {
-					skin.font && CT.dom.addStyle(null, null, {
-						body: {
-							"font-family": skin.font
-						}
-					});
-					skin.color && CT.dom.addStyle(null, null, {
-						body: {
-							color: skin.color
-						}
-					});
-					skin.background && CT.dom.addStyle(null, null, {
-						body: {
-							background: skin.background
-						}
-					});
-					skin.img && CT.dom.addStyle(null, null, {
-						body: {
-							background: "url(" + skin.img + ")",
-							"background-size": "cover"
-						}
-					});
-					skin.css && CT.dom.addStyle(skin.css);
-					if (skin.title) {
-						CT.dom.setContent("feed_title", skin.title);
-						CT.dom.setContent(CT.dom.tag("title")[0], skin.title);
-					}
-					if (skin.chatter) {
-						var chchunk = 6, chparams = {
-							uid: uid,
-							offset: 0,
-							number: chchunk,
-							gtype: "media",
-							mtype: "comment"
-						};
-						CT.dom.id("feed").classList.add("w4-5");
-						CT.dom.show("feed_chatter");
-						CT.net.post("/get", chparams, null, function(items) {
-							var fcnode = CT.dom.id("feed_chatter"),
-								fcrefiller = CT.dom.div();
-							fcrefiller.on("visible", function() {
-								chparams.offset += chchunk;
-								CT.dom.remove(fcrefiller);
-								CT.net.post("/get", chparams, null, function(cz) {
-									for (var i = 0; i < cz.length; i++)
-										fcnode.addNode(cz[i], true);
-									if (cz.length == chchunk)
-										CT.dom.addContent(fcnode, fcrefiller);
-								});
-							});
-							CAN.widget.stream.comment(fcnode, null,
-								items.reverse(), false, true, "full");
-							CT.dom.addContent(fcnode, fcrefiller);
-						});
-					}
-					if (skin.chat) {
-						CT.dom.loadAllNode(true);
-						CAN.frame.loadSiteWideChat();
-					}
-				}
-			}
-			CT.dom.addContent("feed", data.map(CAN.media.loader.contentNode));
-			empty = data.length != chunk;
-			if (empty)
-				CT.dom.remove("refiller");
-			else if (!offset) { // first request
-				CT.dom.addContent(document.body, CT.dom.div(null, null, "refiller", {
-					onvisible: refill
-				}));
-			}
-			offset += chunk;
-		}
+		cb: process
 	});
 };
 CT.onload(refill);
