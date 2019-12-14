657125d4f4ba9ae7f3a87dee2dc196025b90975b
bubbleboy14 <mario.balibrera@gmail.com>
2019-12-13 21:51:57 -0800

    feed page loader: rmed (probs) unused slider widget import

--- ctnet/js/pages/feed.js
+++ ctnet/js/pages/feed.js
@@ -10,7 +10,6 @@ CT.require("CAN.cookie");
 CT.require("CAN.frame");
 CT.require("CAN.session");
 CT.require("CAN.widget.conversation");
-CT.require("CAN.widget.slider");
 CT.require("CAN.widget.stream");
 CT.require("CAN.media.all");
 
