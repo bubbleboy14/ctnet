5e662d4c5c3e1ab81e048a0a0cfdb2fbecd2a2c2
bubbleboy14 <mario.balibrera@gmail.com>
2021-10-02 20:15:36 -0700

    CAN.media.loader ytUnthumb() / linkProcessor(): stopPropagation() on thumb click

--- ctnet/js/CAN/media/loader.js
+++ ctnet/js/CAN/media/loader.js
@@ -385,11 +385,12 @@ CAN.media.loader = {
 		});
 		return n;
 	},
-	"ytUnthumb": function(key) {
+	"ytUnthumb": function(key, e) {
 		var n = CT.dom.id(key);
 		n.style.height = n.firstChild.clientHeight + "px";
 		n.classList.remove("vidthumb");
 		n.innerHTML = CT.video.full(CT.video.videoData("https://youtube.com?v=" + key.slice(0, -4)));
+		e && e.stopPropagation();
 	},
 	"_linkFlags": {
 		"thought": "community.html#!Stream",
@@ -408,7 +409,7 @@ CAN.media.loader = {
 		if (url.indexOf("youtube.com") != -1) {
 			var key = url.split("v=")[1].split("&")[0],
 				keyran = key + Math.floor(1000 + Math.random() * 1000);
-			return '<div class="vidthumb" id="' + keyran + '"><img class="pointer" src="https://img.youtube.com/vi/' + key + '/0.jpg" onclick="CAN.media.loader.ytUnthumb(\'' + keyran + '\')"></div>';
+			return '<div class="vidthumb" id="' + keyran + '"><img class="pointer" src="https://img.youtube.com/vi/' + key + '/0.jpg" onclick="CAN.media.loader.ytUnthumb(\'' + keyran + '\', arguments[0])"></div>';
 		}
 		// thoughts, events, cases, memes, opinions, papers
 		for (var i = 0; i < CAN.media.loader._linkTypes.length; i++) {
