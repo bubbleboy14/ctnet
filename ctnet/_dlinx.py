a0e8064a06ca0e32334291ffcbc1f7e41077e596
bubbleboy14 <mario.balibrera@gmail.com>
2023-10-24 21:42:36 -0700

    basic _dlinx handler: standard /_dlinx token acquisition requests via succeed(); /?.*-style meta-injecting js redirection via redirect() w/ metas; needs Dlink.item setting

---
+++ ctnet/_dlinx.py
@@ -0,0 +1,17 @@
+from cantools.web import respond, cgi_get, succeed, redirect, metized
+from cantools.util import token
+from model import Dlink
+
+def response():
+	p = cgi_get("p", required=False)
+	if p:
+		dlink = Dlink.query(Dlink.path == p).get()
+		if not dlink: # TODO: set item!
+			dlink = Dlink()
+			dlink.path = p
+			dlink.token = token()
+		succeed(dlink.data())
+	dlink = Dlink.query(Dlink.token == cgi_get("t")).get()
+	redirect(dlink.path, metas=metized(dlink.metas()))
+
+respond(response)
